170e1f11fe5a2a8dcb971e37c138301033dd212e
Midhun Mohan <k.midhun@outlook.com>
2023-03-30 15:44:09 -0400

    weekly update of tasks

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 import streamlit as st
 import pandas as pd
 import requests
-from bs4 import BeautifulSoup
+from datetime import datetime
 
-# Send a request to the website
-url = 'https://freakqency.pythonanywhere.com'
-response = requests.get(url)
+#function to get the data from the url every Sunday
+def get_data():
+    # Send a request to the website
+    url = 'https://freakqency.pythonanywhere.com'
+    response = requests.get(url)
 
-responses = response.content.decode('utf-8')
+    responses = response.content.decode('utf-8')
 
-# Split the string into rows and columns
-rows = responses.split('\n')
-cols = rows[0].split(':')
-data = [r.split(':') for r in rows if r]
+    # Split the string into rows and columns
+    rows = responses.split('\n')
+    cols = rows[0].split(':')
+    data = [r.split(':') for r in rows if r]
 
-# Create a Pandas DataFrame
-df = pd.DataFrame(data, columns=["Assigned to","Task"])
+    # Create a Pandas DataFrame
+    df = pd.DataFrame(data, columns=["Assigned to","Task"])
+    df.to_csv('tasks.csv', index=False)
+
+if datetime.today().weekday() == 0:
+    get_data()
+
+# Read the data from the CSV file
+df = pd.read_csv('tasks.csv')
 
 # Show the updated DataFrame after the user checks the checkboxes
 st.write("Here's the updated task list:")
